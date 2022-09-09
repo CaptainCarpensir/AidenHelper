db83dd4b0d84da40c0d7e07d278f15a27b33cc94
amctyg <amctyg@umsystem.edu>
2022-09-08 23:08:56 -0500

    Adjust loenn support
    Still not quite working correctly

--- Loenn/entities/linkedSinkingPlatform.lua
+++ Loenn/entities/linkedSinkingPlatform.lua
@@ -10,19 +10,23 @@ for _, texture in ipairs(textures) do
     textureOptions[utils.titleCase(texture)] = texture
 end
 
-local sinkingPlatform = {}
+local linkedSinkingPlatform = {}
 
-sinkingPlatform.name = "AidenHelper/LinkedSinkingPlatform"
-sinkingPlatform.depth = 1
-sinkingPlatform.fieldInformation = {
+linkedSinkingPlatform.name = "AidenHelper/LinkedSinkingPlatform"
+linkedSinkingPlatform.depth = 1
+linkedSinkingPlatform.nodeLimits = {1,1}
+linkedSinkingPlatform.fieldInformation = {
     texture = {
         options = textureOptions
     }
 }
-sinkingPlatform.placements = {}
+
+linkedSinkingPlatform.placements = {
+    name = "linkedSinkingPlatform",
+}
 
 for i, texture in ipairs(textures) do
-    sinkingPlatform.placements[i] = {
+    linkedSinkingPlatform.placements[i] = {
         name = texture,
         data = {
             width = 16,
@@ -31,16 +35,12 @@ for i, texture in ipairs(textures) do
     }
 end
 
-function sinkingPlatform.sprite(room, entity)
+function linkedSinkingPlatform.sprite(room, entity)
     local sprites = {}
 
-    -- Prevent visual oddities with too long lines
     local x, y = entity.x or 0, entity.y or 0
-    local nodeY = room.height - 2
-
-    if y > nodeY then
-        nodeY = y
-    end
+    local nodes = entity.nodes or {{x = x, y = y+16}}
+    local nodeY = nodes[1].y
 
     resortPlatformHelper.addConnectorSprites(sprites, entity, x, y, x, nodeY)
     resortPlatformHelper.addPlatformSprites(sprites, entity, entity)
@@ -48,6 +48,13 @@ function sinkingPlatform.sprite(room, entity)
     return sprites
 end
 
-sinkingPlatform.selection = resortPlatformHelper.getSelection
+function linkedSinkingPlatform.nodeSprite(room, entity, node)
+    local normalizedNode = node
+    normalizedNode.x = entity.x
 
-return sinkingPlatform
+    return resortPlatformHelper.addPlatformSprites({}, entity, normalizedNode)
+end
+
+linkedSinkingPlatform.selection = resortPlatformHelper.getSelection
+
+return linkedSinkingPlatform
